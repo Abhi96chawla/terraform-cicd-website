6ec0b9a34ce7d89aed23d094ec7734a8479a39ff
Abhi96chawla <chawlaa019@gmail.com>
2024-12-11 14:13:54 -0500

    Update configure-name-profile.sh

--- cicd/configure-name-profile.sh
+++ cicd/configure-name-profile.sh
@@ -4,15 +4,9 @@
 set -eu
 
 # Configure AWS named profile
-<<<<<<< HEAD
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID 
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY 
 aws configure set region $AWS_REGION 
-=======
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region $AWS_REGION
->>>>>>> 59a8ab0f89b67d534bc219f45a53ab77021cc8a5
 
 # Verify that the profile is configured
 #aws configure list --profile $PROFILE_NAME
